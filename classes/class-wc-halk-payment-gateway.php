0ad55bbef54cc523ddc7517437c070b483603ac1
m1tk00 <m1tk00@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2018-09-04 11:30:26 +0000

    tagging version 1.1
    git-svn-id: https://plugins.svn.wordpress.org/woo-halkbank-payment-gateway/trunk@1935538 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- classes/class-wc-halk-payment-gateway.php
+++ classes/class-wc-halk-payment-gateway.php
@@ -6,7 +6,7 @@
  *
  * @class 		WC_Halk_Payment_Gateway
  * @extends		WC_Payment_Gateway
- * @version		1.0.0
+ * @version		1.1.0
  * @package		WooCommerce/Classes/Payment
  * @author 		Mitko Kockovski
  */
